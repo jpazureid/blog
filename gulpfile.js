b2b9f98e1de76d44dc6fcf34cd656350b5fa7aa4
Wataru Haniyama <haniyama.wataru+work@gmail.com>
2019-03-20 18:33:55 +0900

    move hexo files to root

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const gulp = require("gulp");
+const replace = require("gulp-string-replace");
+const del = require('del');
+const path = require('path');
+
+const sourceFolders = ["active-directory-federation-service/", "azure-active-directory/", "azure-active-directory-connect/"];
+const markdownFiles = sourceFolders.map(dir => path.join(dir, "**/*.md"));
+const imageFiles = sourceFolders.map(dir => path.join(dir, "**/*.+(jpg|jpeg|png|gif)"));
+const sourceFiles = sourceFolders.map(dir => path.join(dir, "**/*"));
+
+const outputPath = "source/_posts/";
+const Hexo = require("hexo");
+const hexo = new Hexo(process.cwd(), {});
+
+gulp.task("server", function (cb) {
+  hexo
+    .init()
+    .then(function () {
+      return hexo.call("server", {});
+    })
+    .then(function () {
+      return hexo.exit();
+    })
+    .then(function () {
+      return cb();
+    })
+    .catch(function (err) {
+      console.log(err);
+      hexo.exit(err);
+      return cb(err);
+    });
+});
+
+gulp.task("deploy", function (cb) {
+  hexo
+    .init()
+    .then(function () {
+      return hexo.call("clean", {});
+    })
+    .then(function () {
+      return hexo.call("deploy", {});
+    })
+    .then(function () {
+      return hexo.exit();
+    })
+    .then(function () {
+      return cb();
+    })
+    .catch(function (err) {
+      console.log(err);
+      hexo.exit(err);
+      return cb(err);
+    });
+});
+
+gulp.task('cleanOutputPath', function () {
+  return del([
+    path.join(outputPath, "/**/*")
+  ]);
+});
+
+gulp.task("open", () => {
+  return gulp.src("./").pipe(exec("open http://localhost:4000"));
+});
+
+gulp.task("copyMarkdown", () => {
+  return (
+    gulp
+    .src(markdownFiles)
+    //fix absolute path image
+    .pipe(
+      replace(/!\[.*\]\(.*\/(.*)\)/g, (match, p1, offset, string) => {
+        return `{% asset_img ${p1} %}`;
+      })
+    ).pipe(
+      // delete first h1 header
+      replace(/^# .*/m, "")
+    )
+    .pipe(gulp.dest(outputPath))
+  );
+});
+
+gulp.task("copyImage", () => {
+  return gulp.src(imageFiles).pipe(gulp.dest(outputPath));
+});
+
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("cleanOutputPath" ,"copyMarkdown", "copyImage"), "server")
+);
+
+gulp.task(
+  "publish",
+  gulp.series(gulp.parallel("copyMarkdown", "copyImage"), "deploy")
+);
